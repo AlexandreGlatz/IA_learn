bde328f25457a829e87364d11c461f4eafd83c3e
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2024-11-15 15:23:44 +0100

    [ADD] Fixes

--- LightEngine-master/LightEngine/Zombie.cpp
+++ LightEngine-master/LightEngine/Zombie.cpp
@@ -1,1 +1,13 @@
 #include "Zombie.h"
+
+Zombie::Zombie(float radius, sf::Color color) : Entity(radius, color)
+{
+}
+
+Zombie::~Zombie()
+{
+}
+
+void Zombie::OnUpdate()
+{
+}
--- LightEngine-master/LightEngine/Zombie.h
+++ LightEngine-master/LightEngine/Zombie.h
@@ -1,10 +1,13 @@
 #pragma once
-class Zombie
+#include "Entity.h"
+#include <SFML/Graphics.hpp>
+class Zombie : public Entity
 {
 public:
-	Zombie();
+	Zombie(float radius = 50, sf::Color color = sf::Color::Red);
 	~Zombie();
 
+	void OnUpdate() override;
 private:
 
 
